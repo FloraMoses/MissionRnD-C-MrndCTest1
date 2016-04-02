40a562a0d0cc687a0872dd2177a52911ea6472f0
FloraMoses <FloraMoses@missionrnd.com>
2016-04-02 21:02:24 +0530

    Submit Commit

--- src/ArraySequences.cpp
+++ src/ArraySequences.cpp
@@ -5,8 +5,8 @@ Copy indexes where each sequence starts and ends into an Array and return it.
 
 Note : Each sequence will be of atleast LENGTH 3,and The sequences can be overlapping
 
-Ex : 1, 3, 5, 7, 2, 4, 8, 16 18 20, output is [0, 3, 7, 9, 4, 7]
-Ex : 1 ,2, 4, 8, 12, 16, 21, 26     output is [2, 5, 5, 7, 0, 3]
+Ex : 1, 3, 5, 7, 2, 4, 8, 16 18 20, output is     [0, 3, 7, 9, 4, 7]
+Ex : 1 ,2, 4, 8, 12, 16, 21, 26     output is     [2, 5, 5, 7, 0, 3]
 Ex : 10 15 20 30 50 70 100 200 400 800  output is [0, 2, 3, 5, 6, 9]
 
 The final_arr[0], final_arr[1] values in final array  
@@ -30,8 +30,58 @@ Difficulty : Medium
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-
-int * find_sequences(int *arr, int len){
-	//Return final array which has 6indexes [AP1_S,AP1_E,AP2_S,AP2_E,GP1_S,GP2_E]
-	return NULL;
+int * find_sequences(int *arr, int len) {
+	if (arr == NULL || len <= 0)
+		return NULL;
+	int n = len, i, flag = 0;
+	int result[6] = { 0 }, diff1 = arr[1] - arr[0], diff2, ratio1, ratio2 = 0;
+	for (i = 0; i < len - 1; i++) {
+		diff2 = arr[i + 1] - arr[i];
+		if (diff1 == diff2) {
+			if (flag == 0) {
+				flag = 1;
+				result[0] = i;
+			}
+			else {
+				result[1] = i + 1;
+			}
+		}
+		else
+			break;
+		diff1 = arr[i + 1] - arr[i];
+	}
+	flag = 0;
+	for (; i < len - 1; i++) {
+		diff2 = arr[i + 1] - arr[i];
+		if (diff1 == diff2) {
+			if (flag == 0) {
+				flag = 1;
+				result[2] = i - 1;
+				result[3] = i + 1;
+			}
+			else {
+				result[3] = i + 1;
+			}
+		}
+		diff1 = arr[i + 1] - arr[i];
+	}
+	flag = 0;
+	for (i = 0; i < len - 1; i++) {
+		ratio1 = arr[i + 1] / arr[i];
+		if (ratio1 == ratio2 && (arr[i] - arr[i - 1]) != 1) {
+			if (flag == 0) {
+				flag = 1;
+				result[4] = i - 1;
+				result[5] = i + 1;
+			}
+			else {
+				result[5] = i + 1;
+			}
+		}
+		ratio2 = arr[i + 1] / arr[i];
+	}
+	for (int i = 0; i < 6; i++) {
+		printf("%d \n", result[i]);
+	}
+	return result;
 }
--- src/BetweenDays.cpp
+++ src/BetweenDays.cpp
@@ -27,8 +27,93 @@ struct node{
 	int data;
 	struct node *next;
 };
-
-
+int isLeap(int);
+int abs(int, int);
+int isSame(struct node*, struct node*);
+int isConsecutive(struct node*, struct node*);
 int between_days(struct node *date1head, struct node *date2head){
-	return -1;
+	if (date1head == NULL || date2head == NULL) {
+		return -1;
+	}
+	if (isSame(date1head, date2head) || isConsecutive(date1head, date2head)) {
+		return 0;
+	}
+	struct node *head1 = date1head;
+	struct node *head2 = date2head;
+	int day1 = (date1head->data * 10) + date1head->next->data;
+	int day2 = (date2head->data * 10) + date2head->next->data;
+	date1head = date1head->next->next;
+	date2head = date2head->next->next;
+	int month1 = (date1head->data * 10) + date1head->next->data;
+	int month2 = (date2head->data * 10) + date2head->next->data;
+	date1head = date1head->next->next;
+	date2head = date2head->next->next;
+	int year1 = (date1head->data * 1000) + (date1head->next->data * 100) + (date1head->next->next->data * 10) + date1head->next->next->next->data;
+	int daysInMonth[12];
+	int year2 = (date2head->data * 1000) + (date2head->next->data * 100) + (date2head->next->next->data * 10) + date2head->next->next->next->data;
+	if (isLeap(year2)) {
+		int days[] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		for (int i = 0; i < 12; i++) {
+			daysInMonth[i] = days[i];
+		}
+	}
+	else {
+		int days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		for (int i = 0; i < 12; i++) {
+			daysInMonth[i] = days[i];
+		}
+	}
+	if (year2 == year1) {
+		if (month2 > month1) {
+			int result = daysInMonth[month1 - 1];
+			result += (abs(day1, day2));
+		}
+		else if (month1 == month2) {
+			return (day2 - day1) - 1;
+		}
+		else {
+			return -1;
+		}
+	}
+	else {
+		return -1;
+	}
+}
+int abs(int num1, int num2) {
+	if (num1 > num2) {
+		return num1 - num2;
+	}
+	return num2 - num1;
+}
+int isLeap(int year) {
+	if (year % 400 == 0) {
+		return 1;
+	}
+	else if (year % 100 == 0) {
+		return 0;
+	}
+	else if (year % 4 == 0) {
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+int isConsecutive(struct node *date1head, struct node *date2head) {
+	int date1, date2;
+	date1 = (date1head->data * 10) + date1head->next->data;
+	date2 = (date1head->data * 10) + date1head->next->data;
+	if (date1 - date2 == 1 || date2 - date1 == 1) {
+		return 1;
+	}
+	return 0;
+}
+int isSame(struct node *date1head, struct node *date2head) {
+	while (date1head != NULL) {
+		if (date1head->data != date2head->data)
+			return 0;
+		date1head = date1head->next;
+		date2head = date2head->next;
+	}
+	return 1;
 }
